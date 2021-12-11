2d420c3238ec730a4c6ea5eddfdaf63cca45ca15
Wellington Nogueira Elizeu da Conceição <wellingtoncw7@gmail.com>
2021-12-11 13:29:19 -0300

    Pilotando a DOM

--- Pilotando com a DOM/script.js
+++ Pilotando com a DOM/script.js
@@ -1,2 +1,15 @@
+// pegando o elemento pelo ID
 const element = document.getElementById('blog-title')
 console.log(element)
+
+//pegando elemento pelo ClassName
+const element1 = document.getElementsByClassName('one')
+console.log(element1)
+
+//pegando pelo nome da tag HTML
+const element3 = document.getElementsByTagName('div')
+console.log(element3)
+
+// querySelector - pega o primeiro seletor que encontrar na página
+const element4 = document.querySelector('.one')
+console.log(element4)
